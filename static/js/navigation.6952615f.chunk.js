b7853a90a2553d010d79d5f4fdef3144240cc87d
PiekharievaK <86105196+PiekharievaK@users.noreply.github.com>
2022-05-05 00:33:27 +0000

    Deploying to gh-pages from @ PiekharievaK/goit-react-hw-05-movies@e0cc0c038aba89f3a278fe021085e68ca5871fa9 🚀

---
+++ static/js/navigation.6952615f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[384],{4494:function(e,s,n){n.r(s),n.d(s,{default:function(){return a}});var i=n(501),t=n(184);function a(){return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)(i.OL,{to:"/",className:"NavLink",style:{margin:"15px"},children:"Home"}),(0,t.jsx)(i.OL,{to:"/movies",className:"NavLink",children:"Movies"})]})}}}]);
+//# sourceMappingURL=navigation.6952615f.chunk.js.map
